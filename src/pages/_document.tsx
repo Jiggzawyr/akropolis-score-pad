0d721b606a2967be6ce5833723ea09927f2dccd6
Jiggzawyr <martinstojanovic96@gmail.com>
2023-10-08 17:57:31 +0200

    seo optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Akropolis score pad</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="description" content="Akropolis board game scoring pad web app."></meta>
+        <meta name="keywords" content="Akropolis, board game, game, board game scoring, score pad, scorekeeping, score sheet"></meta>
+      </Head>
       <body 
         className="bg-[url('/img/background.jpg')] text-sm lg:text-xl"
       >
